5efddc687a58c9cd3f2303b86df57de2f0dc414e
masterthuku <peterthuku706@gmail.com>
2025-01-05 02:46:44 +0300

    first update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home () {
   return (
-    <div className="flex h-screen items-center justify-center bg-amber-100">
+    <div className="flex-center h-screen">
       <h1 className="text-3xl  font-bold text-brand" >Store It - The only online storage you need </h1>
     </div>
   )
---
+++ lib/appwrite/index.ts
@@ -0,0 +1,8 @@
+import { Client } from "node-appwrite"
+import { appwriteConfig } from "./config"
+
+export const createSessionClient = async () => {
+    const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+}
